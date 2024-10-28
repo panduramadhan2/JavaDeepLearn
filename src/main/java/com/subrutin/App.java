813da4ec029318f154e1a07bf766bdbb649bfb60
panduramadhan2 <panduramadhan2@gmail.com>
2024-10-28 14:31:41 +0700

    checkpoint inject Bean 29

--- src/main/java/com/subrutin/App.java
+++ src/main/java/com/subrutin/App.java
@@ -1,6 +1,9 @@
 package com.subrutin;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.subrutin.domain.Author;
+import com.subrutin.domain.Book;
 
 /**
  * Hello world!
@@ -9,8 +12,13 @@ import com.subrutin.domain.Author;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Author author = new Author();
-		author.setId(1L);
-		author.setName("Karen Armstrong");
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("application-context.xml");
+		Author author = (Author) appContext.getBean("author");
+		System.out.println(author.getId());
+		System.out.println(author.getName());
+
+		Book book = (Book) appContext.getBean("book");
+		System.out.println(book.getTitle());
+		System.out.println(book.getAuthor().getName());
 	}
 }
--- src/main/java/com/subrutin/domain/Book.java
+++ src/main/java/com/subrutin/domain/Book.java
@@ -8,4 +8,20 @@ public class Book {
 		this.author = author;
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
 }
