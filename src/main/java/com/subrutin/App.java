4aad151dfecfe7325c32058f2a87935b50cc86e0
panduramadhan2 <panduramadhan2@gmail.com>
2024-10-29 16:08:33 +0700

    checkpoint 31 membuat banyak Bean dari satu class

--- src/main/java/com/subrutin/App.java
+++ src/main/java/com/subrutin/App.java
@@ -13,11 +13,11 @@ public class App {
 	public static void main(String[] args) {
 //		System.out.println("Hello World!");
 		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("application-context.xml");
-		Author author = (Author) appContext.getBean("author");
+		Author author = (Author) appContext.getBean("author2");
 		System.out.println(author.getId());
 		System.out.println(author.getName());
 
-		Book book = (Book) appContext.getBean("book");
+		Book book = (Book) appContext.getBean("book2");
 		System.out.println(book.getTitle());
 		System.out.println(book.getAuthor().getName());
 
