6f211957445d7c12c7e298b79e390f093bce20a6
panduramadhan2 <panduramadhan2@gmail.com>
2024-11-01 16:52:20 +0700

    checkpoint 34

--- src/main/java/com/subrutin/App.java
+++ src/main/java/com/subrutin/App.java
@@ -2,24 +2,24 @@ package com.subrutin;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.subrutin.domain.Author;
-import com.subrutin.domain.Book;
+import com.subrutin.service.EmailService;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
-//		System.out.println("Hello World!");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Program berjalan");
 		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("application-context.xml");
-		Author author = (Author) appContext.getBean("author2");
-		System.out.println(author.getId());
-		System.out.println(author.getName());
-
-		Book book = (Book) appContext.getBean("book2");
-		System.out.println(book.getTitle());
-		System.out.println(book.getAuthor().getName());
-
+//		Author author = (Author) appContext.getBean("author2");
+//		System.out.println(author.getId());
+//		System.out.println(author.getName());
+//
+//		Book book = (Book) appContext.getBean("book2");
+//		System.out.println(book.getTitle());
+//		System.out.println(book.getAuthor().getName());
+		EmailService emailService = (EmailService) appContext.getBean("emailService");
+		emailService.sendMail("destination@gmail.com", "Your OTP", "Your OTP is 12345");
 	}
 }
